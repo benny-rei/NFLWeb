6a8767e7202bbeee7f28b553e06a2224c0aca754
Benny <Benny@192.168.0.114>
2017-04-03 15:27:57 +0200

    Signed-off-by: Benny <Benny@192.168.0.114>

---
+++ NFL-Web/src/Model/DBManager.java
@@ -0,0 +1,36 @@
+package Model;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+
+/**
+ * In dieser Klasse befinden sich die Methoden, welche Daten aus der Datenbank abfragt, ändert.
+ * @author Reichhold & Schlögl
+ *
+ */
+public class DBManager {
+	
+	private static DBManager instance = null;
+	private Connection conn;
+	
+	public DBManager() throws ClassNotFoundException, SQLException{	
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/NFL", "root", "");
+
+	}
+
+	public static DBManager Instance() throws ClassNotFoundException, SQLException{
+		if(instance == null){
+			instance = new DBManager();
+		}
+		return instance;
+	}
+	
+	
+
+}
---
+++ NFL-Web/src/Model/Teams.java
@@ -0,0 +1,98 @@
+package Model;
+
+public class Teams {
+	
+	private int spielerID;
+	private String vorname, nachname, geburtsdatum;
+	private int superBowlErfolge, championshipTitel, prowbowlWahlen, seasonMVP, superbowlMVP;
+	
+	public Teams(int spielerID, String vorname, String nachname, String geburtsdatum, int superBowlErfolge,
+			int championshipTitel, int prowbowlWahlen, int seasonMVP, int superbowlMVP) {
+		super();
+		this.spielerID = spielerID;
+		this.vorname = vorname;
+		this.nachname = nachname;
+		this.geburtsdatum = geburtsdatum;
+		this.superBowlErfolge = superBowlErfolge;
+		this.championshipTitel = championshipTitel;
+		this.prowbowlWahlen = prowbowlWahlen;
+		this.seasonMVP = seasonMVP;
+		this.superbowlMVP = superbowlMVP;
+		
+	}
+
+	public int getSpielerID() {
+		return spielerID;
+	}
+
+	public void setSpielerID(int spielerID) {
+		this.spielerID = spielerID;
+	}
+
+	public String getVorname() {
+		return vorname;
+	}
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+	public String getNachname() {
+		return nachname;
+	}
+
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+
+	public String getGeburtsdatum() {
+		return geburtsdatum;
+	}
+
+	public void setGeburtsdatum(String geburtsdatum) {
+		this.geburtsdatum = geburtsdatum;
+	}
+
+	public int getSuperBowlErfolge() {
+		return superBowlErfolge;
+	}
+
+	public void setSuperBowlErfolge(int superBowlErfolge) {
+		this.superBowlErfolge = superBowlErfolge;
+	}
+
+	public int getChampionshipTitel() {
+		return championshipTitel;
+	}
+
+	public void setChampionshipTitel(int championshipTitel) {
+		this.championshipTitel = championshipTitel;
+	}
+
+	public int getProwbowlWahlen() {
+		return prowbowlWahlen;
+	}
+
+	public void setProwbowlWahlen(int prowbowlWahlen) {
+		this.prowbowlWahlen = prowbowlWahlen;
+	}
+
+	public int getSeasonMVP() {
+		return seasonMVP;
+	}
+
+	public void setSeasonMVP(int seasonMVP) {
+		this.seasonMVP = seasonMVP;
+	}
+
+	public int getSuperbowlMVP() {
+		return superbowlMVP;
+	}
+
+	public void setSuperbowlMVP(int superbowlMVP) {
+		this.superbowlMVP = superbowlMVP;
+	}
+	
+	
+	
+}
